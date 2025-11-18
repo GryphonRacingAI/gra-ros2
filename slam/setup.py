cf3e9c8bd835ea0b04cc6200ec2c1da572c63d53
Hasham Ahmad [fy23ha] <fy23ha@uol-pc-055671.leeds.ac.uk>
2025-11-18 19:06:57 +0000

    fastslam added

--- slam/setup.py
+++ slam/setup.py
@@ -1,30 +1,34 @@
-from setuptools import setup
-import os
+import os                       
 from glob import glob
+from setuptools import setup, find_packages    
 
-package_name = 'fastslam_ros'
+package_name = 'slam'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+    packages=find_packages(exclude=['test']),
+
+	data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*'))
     ],
-    # Numpy is explicitly listed here and must be installed in the environment (via rosdep or pip)
-    install_requires=['setuptools', 'numpy'], 
+    include_package_data=True,
+
+    # Dependencies required at installation time
+    install_requires=['setuptools', 'numpy', 'pycuda', 'common_msgs'],
     zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='user@todo.todo',
+    maintainer='Hasham Ahmad',
+    maintainer_email='hashamahmad1818@gmail.com',
     description='ROS 2 wrapper for FastSLAM 1.0 GPU',
     license='MIT',
     tests_require=['pytest'],
+    
     entry_points={
         'console_scripts': [
-            # This line makes your file executable via 'ros2 run fastslam_ros fastslam_node'
-            'fastslam_node = fastslam_ros.fastslam_node:main'
+            'fastslam_node = fastslam64.fastslam_node:main'
         ],
     },
 )
+
